60f3abb801b0fed18819010685d282c0a420b4fc
abuabdul <abuabdul86@hotmail.com>
2017-02-11 23:02:05 +0530

    Add Omage modules newly

---
+++ omage-web/src/main/java/org/b2oplus/omage/OmageApplication.java
@@ -0,0 +1,12 @@
+package org.b2oplus.omage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmageApplication.class, args);
+	}
+}
